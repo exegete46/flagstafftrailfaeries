cb0b108355860c3d7a17fcb69ecc7fe1e25734c3
Jack M <exegete@mac.com>
2019-05-20 18:56:16 -0700

    Remove server.sh.

--- server.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# TODO: Remove these once Cloud9 fixes environment bugs.
-export GEM_HOME=/usr/local/rvm/gems/ruby-2.3.0
-export GEM_PATH=/usr/local/rvm/gems/ruby-2.3.0:/usr/local/rvm/gems/ruby-2.3.0@global
-
-bundle check
-
-bundle exec jekyll server --host $IP --port $PORT --baseurl ''
